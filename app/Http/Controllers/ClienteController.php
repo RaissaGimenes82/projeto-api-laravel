c8cfb537f8218515aa9dee9032c843d784dd7ffe
RaissaGimenes <raissagimenes@rede.ulbra.br>
2024-10-08 15:27:18 -0300

    Added project files

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    public function listar()
+    {
+        $array = [
+            "nome" => "Nome teste"
+        ];
+        return response ()->json($array, 200);
+    }
+}
